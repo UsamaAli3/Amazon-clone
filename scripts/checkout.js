34d43c54575e95d2d6844f8ea0519723f155926d
Usama Ali <132250750+UsamaAli3@users.noreply.github.com>
2024-05-19 18:50:12 +0500

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,4 @@
+import { renderOrderSummery } from "../scripts/checkout/ordersummery.js";
+import { renderPaymentSummery } from "../scripts/checkout/paymentsummery.js";
+renderOrderSummery();
+renderPaymentSummery();
