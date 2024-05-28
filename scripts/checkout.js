d34409a00bbf10c53cc3862f377e3f3a1f9f3e1c
Usama Ali <imusamaali7@gmail.com>
2024-05-28 16:58:58 +0500

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderOrderSummery } from "../scripts/checkout/ordersummery.js";
 import { renderPaymentSummery } from "../scripts/checkout/paymentsummery.js";
-import"../data/cart-oop.js"
+import"../data/cart-class.js"
 renderOrderSummery();
 renderPaymentSummery();
